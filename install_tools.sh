4ea303f98e9c747739ee98a12f61a05b6317ab1c
Alexander Trojnin <trojninalex@linux-workstation>
2021-07-25 16:11:36 +0300

    Update docs

--- install_tools.sh
+++ install_tools.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
-printf "\nInstall Cross Develop Tools\n\n" 
+printf "\nInstall Cross Develop Tools\n" 
 sudo apt install crossbuild-essential-armel crossbuild-essential-armhf crossbuild-essential-arm64 
 
-printf "\nInstall Develop Application\n\n"
-sudo apt install emacs-nox git 
+printf "\nInstall Develop Application\n"
+sudo apt install emacs-nox git lynx
+
+printf "\nInstall QEMU\n"
+sudo apt install qemu qemu-system-x86 qemu-system-arm qemu-user-static qemu-user qemu-efi qemu-efi-arm qemu-efi-aarch64 qemu-system-mips 
 
 printf "\n"
